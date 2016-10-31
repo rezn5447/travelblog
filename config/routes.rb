a0dc5c647d3a6d690f44fd19ddff71a832581cb7
rezn5447 <rezn5447@gmail.com>
2016-10-30 17:23:23 -0700

    sass practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  devise_for :users
+  devise_for :users 
   
+
   resources :posts
+ 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
