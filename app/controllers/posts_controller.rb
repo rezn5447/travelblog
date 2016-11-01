f307aabaf3ba19f21666a824bc180321c6089f9f
rezn5447 <rezn5447@gmail.com>
2016-11-01 09:52:32 -0700

    gitignore

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @post = Post.find(params[:id])
   end
@@ -8,5 +10,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root
+  end
+
+
+
+
+
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:title, :body, current_user.id)
   end
 end
